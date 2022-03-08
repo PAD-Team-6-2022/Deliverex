ed3ec135983067c02429b3ac75d29501f60f4bdd
Milan de Kruijf <milandekruijf@re-boyds-145-109-134-96.wireless.hva.nl>
2022-03-08 15:42:23 +0100

    forcing database to sync when in development mode

--- client/assets/js/pages/dashboard/overview.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "../../router.js";
-
-const router = new Router("test");
-
-router.set("/test", "test");
--- server/db/connection.js
+++ server/db/connection.js
@@ -6,25 +6,29 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  logging: false
+  logging: false,
 });
 
 const init = async () => {
   try {
     await sequelize.authenticate();
     console.log("The database was succesfully connected");
-  
-    if(process.env.NODE_ENV === "development") {
-      console.log("Running in development mode, attempting to sync database...");
-  
-      await sequelize.sync({ alter: true });
-  
+
+    if (process.env.NODE_ENV === "development") {
+      console.log(
+        "Running in development mode, attempting to sync database..."
+      );
+
+      await sequelize.sync({
+        force: process.env.NODE_ENV === "development" ? true : false,
+      });
+
       console.log("All models were succesfully synced with the database");
     }
-  } catch(err) {
+  } catch (err) {
     console.error("Unable to connect to the database:", err);
   }
-}
+};
 
 init();
 
