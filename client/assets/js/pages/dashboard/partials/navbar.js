204abee10ad4247d3ca78e7479c63aea85e8f6a3
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-03-09 14:35:33 +0100

    changed file locations for dashboard partials

--- client/assets/js/pages/dashboard/partials/navbar.js
+++ client/assets/js/pages/dashboard/partials/navbar.js
@@ -3,11 +3,16 @@
  */
 const navToggle = () => {
     const menu = document.querySelector('#mobile-menu');
+    const nav = document.querySelector('#nav');
 
     if(menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
+        nav.classList.add('h-auto');
+        nav.classList.add('md:h-16');
+        
     } else {
         menu.classList.add('hidden');
+        nav.classList.remove('h-auto');
     }
 }
 
