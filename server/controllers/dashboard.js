ae1f627b7676a3ece9371e6295c2cfe7202bd894
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-03-09 14:59:55 +0100

    fixed nav height

--- server/controllers/dashboard.js
+++ server/controllers/dashboard.js
@@ -16,8 +16,46 @@ const DashboardController = {
    * @param {Response} res the response object.
    */
   index: async (req, res) => {
-    const orders = await Order.findAll();
-    res.render("dashboard/overview", { title: "Overzicht - Dashboard" });
+    // const orders = await Order.findAll();
+    const orders = [
+      {
+        code: '1938031',
+        email: 'pete@mail.com',
+        state: 'Deliverd',
+        date: '07-03-2022'
+      },
+      {
+        code: '18751012',
+        email: 'dave@mail.com',
+        state: 'Send',
+        date: '31-02-2022'
+      },
+      {
+        code: '41515105',
+        email: 'mike@mail.com',
+        state: 'Deliverd',
+        date: '07-03-2022'
+      },
+      {
+        code: '17571818',
+        email: 'jim@mail.com',
+        state: 'Failed',
+        date: '31-02-2022'
+      },
+      {
+        code: '15356167',
+        email: 'erick@mail.com',
+        state: 'Deliverd',
+        date: '15-05-2021'
+      },
+      {
+        code: '24627789',
+        email: 'richard@mail.com',
+        state: 'Deliverd',
+        date: '02-05-2022'
+      }
+    ];
+    res.render("dashboard/overview", { title: "Overzicht - Dashboard", orders: orders });
   },
   /**
    * Render the dashboard's create order page. (views/pages/dashboard/create_order.ejs)
