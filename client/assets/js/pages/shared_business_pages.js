5ca4c227b2005cf24fffec9cb19e83e472c83da3
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-03-09 11:11:03 +0100

    Taal veranderd van dashboard pagina en menu toegevoegd

--- client/assets/js/pages/shared_business_pages.js
+++ client/assets/js/pages/shared_business_pages.js
@@ -1,6 +1,9 @@
+/**
+ * Toggle the menu in the navigation bar
+ */
 const navToggle = () => {
     const menu = document.querySelector('#mobile-menu');
-    console.log(menu);
+
     if(menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
