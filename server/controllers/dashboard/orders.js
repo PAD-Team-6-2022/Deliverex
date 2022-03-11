bc9905b6d77a12b3c320c18f7ee74303949cd4eb
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-03-11 20:20:50 +0100

    order detail page made

--- server/controllers/dashboard/orders.js
+++ server/controllers/dashboard/orders.js
@@ -1,9 +1,28 @@
 const auth = require("../../middleware/auth");
+const Order = require("../../models/order");
+const convert = require('convert-units');
 
 const router = require("express").Router();
 
 router.get("/create", auth(true), (req, res) => {
   res.render("dashboard/orders/create", { title: "Create order" });
+});
+
+router.get("/:id", auth(true), async (req, res) => {
+  const id = req.params.id;
+  const order = await Order.findByPk(id);
+
+  // if no order is found redirect it to the dashboard overview
+  if(!order) {
+    res.redirect("/dashboard/overview");
+    return;
+  }
+
+  // covert the weight to the best value "kg" or "g"
+  const convertedWeight = convert(order.weight).from('g').toBest();
+  order.weight = `${Math.round(convertedWeight.val)} ${convertedWeight.unit}`;
+
+  res.render("dashboard/orders/detail", { title: "Order detail", id, order });
 });
 
 module.exports = router;
