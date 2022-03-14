38be07e03b53d5666be55f541de6d03f0b64f7a1
Milan de Kruijf <milandekruijf@Milans-MacBook-Pro.local>
2022-03-14 18:10:46 +0100

    made modals as abstract as possible, now possible to extend

--- client/assets/js/pages/dashboard/overview.js
+++ client/assets/js/pages/dashboard/overview.js
@@ -1,4 +1,5 @@
-import("./partials/navbar.js");
+import "./navbar.js";
+import { openModal } from "../../modal.js";
 
 let nextLink = document.getElementById("nextLink");
 let prevLink = document.getElementById("prevLink");
@@ -37,68 +38,18 @@ document.querySelectorAll("[data-order-code]").forEach((order) => {
   order
     .querySelector("[data-order-delete]")
     .addEventListener("click", async (event) => {
-      console.log(id);
-      document.getElementsByTagName("html")[0].classList.add("overflow-hidden");
-      document.querySelectorAll("[data-modal-overlay]").forEach((overlay) => {
-        overlay.classList.remove("invisible");
-      });
-      document.querySelectorAll("[data-modal-screen]").forEach((screen) => {
-        screen.classList.remove("invisible");
-        screen
-          .querySelector("[data-modal-delete]")
-          .setAttribute("data-order-code", id);
-      });
-      // stop event from going further than current object
-      event.stopPropagation();
-    });
-
-  order.addEventListener("click", async (event) => {
-    window.location.href = `/dashboard/orders/${id}`;
-  });
-});
-
-document.querySelectorAll("[data-modal-screen]").forEach((screen) => {
-  screen
-    .querySelector("[data-modal-cancel]")
-    .addEventListener("click", async () => {
-      document
-        .getElementsByTagName("html")[0]
-        .classList.remove("overflow-hidden");
-      document.querySelectorAll("[data-modal-overlay]").forEach((overlay) => {
-        overlay.classList.add("invisible");
-      });
-      document.querySelectorAll("[data-modal-screen]").forEach((screen) => {
-        screen.classList.add("invisible");
-      });
-    });
-
-  const deleteButton = screen.querySelector("[data-modal-delete]");
-
-  deleteButton.addEventListener("click", async () => {
-    const id = deleteButton.getAttribute("data-order-code");
-
-    document
-      .getElementsByTagName("html")[0]
-      .classList.remove("overflow-hidden");
-
-    document.querySelectorAll("[data-modal-overlay]").forEach((overlay) => {
-      overlay.classList.add("invisible");
-    });
-    document.querySelectorAll("[data-modal-screen]").forEach((screen) => {
-      screen.classList.add("invisible");
-    });
-
-    if (id) {
-      console.log(id);
-      await fetch(`/api/orders/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
+      openModal("delete-order", {
+        actions: {
+          confirm: async () => {
+            await fetch(`/api/orders/${id}`, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+            window.location.reload();
+          },
         },
       });
-      window.location.reload();
-    } else {
-      alert("Failed to delete the order.");
-    }
-  });
+    });
 });
