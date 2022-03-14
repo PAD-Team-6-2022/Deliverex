a6c2909a0e7920885209406316c9ecca5f562c70
Milan de Kruijf <milandekruijf@Milans-MacBook-Pro.local>
2022-03-14 19:59:38 +0100

    complete the entire form with ordering, searching and limits

--- client/assets/js/pages/dashboard/overview.js
+++ client/assets/js/pages/dashboard/overview.js
@@ -1,5 +1,6 @@
 import "./navbar.js";
 import { openModal } from "../../modal.js";
+import "../../ordering.js";
 
 let nextLink = document.getElementById("nextLink");
 let prevLink = document.getElementById("prevLink");
@@ -17,20 +18,6 @@ nextLink.href = "?page=" + (parseInt(page) + 1);
 if (page > 1) {
   prevLink.href = "?page=" + (parseInt(page) - 1);
 }
-
-document.querySelectorAll(".clickableArrow").forEach((arrowButton) => {
-  arrowButton.addEventListener("click", () => {
-    urlParams.set("sort", arrowButton.id);
-    urlParams.set(
-      "order",
-      document.getElementById(arrowButton.id).classList.contains("rotate-180")
-        ? "desc"
-        : "asc"
-    );
-
-    window.location.search = urlParams.toString();
-  });
-});
 
 document.querySelectorAll("[data-order-code]").forEach((order) => {
   const id = order.getAttribute("data-order-code");
