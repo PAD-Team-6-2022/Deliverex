2d67571939965e3b5e343db3e1b0ca26f59ed072
linsset@hva.nl <thomas.linssen@hva.nl>
2022-03-14 18:04:53 +0100

    Fixed filteren bug

--- server/middleware/ordering.js
+++ server/middleware/ordering.js
@@ -3,7 +3,7 @@ const ordering = (defaultSort, defaultOrder) => (req, res, next) => {
   let order = req.query.order || defaultOrder;
 
   // Default to asc if invalid order value
-  if (order !== "asc" || order !== "desc") order = "asc";
+  if (order !== "asc" && order !== "desc") order = "asc";
 
   req.sort = sort;
   req.order = order;
