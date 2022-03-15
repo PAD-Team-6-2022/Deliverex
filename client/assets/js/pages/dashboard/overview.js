cda8840cb7d775abdc6136d8e935f14e1f5f02a9
Milan de Kruijf <milandekruijf@Milans-MacBook-Pro.local>
2022-03-15 16:23:38 +0100

    removed old pagination logic

--- client/assets/js/pages/dashboard/overview.js
+++ client/assets/js/pages/dashboard/overview.js
@@ -3,23 +3,6 @@ import { openModal } from "../../modal.js";
 import "../../ordering.js";
 import "../../pagination.js";
 
-let nextLink = document.getElementById("nextLink");
-let prevLink = document.getElementById("prevLink");
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-let page = urlParams.get("page");
-
-if (page == null) {
-  page = 1;
-}
-
-nextLink.href = "?page=" + (parseInt(page) + 1);
-
-if (page > 1) {
-  prevLink.href = "?page=" + (parseInt(page) - 1);
-}
-
 document.querySelectorAll("[data-order-code]").forEach((order) => {
   const id = order.getAttribute("data-order-code");
 
