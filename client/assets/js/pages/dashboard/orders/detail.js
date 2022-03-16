03fddf661a0bd725b20ebd2ec1b23605e442b33c
Milan de Kruijf <milandekruijf@re-boyds-145-109-138-26.wireless.hva.nl>
2022-03-16 17:35:34 +0100

    temporarily added a very bad way to delete an order from their own page

--- client/assets/js/pages/dashboard/orders/detail.js
+++ client/assets/js/pages/dashboard/orders/detail.js
@@ -1,1 +1,24 @@
 import "../navbar.js";
+import { openModal } from "../../../modal.js";
+
+document.querySelectorAll("[data-order-code]").forEach((order) => {
+  const id = order.getAttribute("data-order-code");
+
+  order
+    .querySelector("[data-order-delete]")
+    .addEventListener("click", async (event) => {
+      openModal("delete-order", {
+        actions: {
+          confirm: async () => {
+            await fetch(`/api/orders/${id}`, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+            window.location.reload();
+          },
+        },
+      });
+    });
+});
