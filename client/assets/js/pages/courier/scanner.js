427084e9f75a6e62300b165e89b1d887681f1381
linsset@hva.nl <thomas.linssen@hva.nl>
2022-04-06 01:17:00 +0200

    Small optimization

--- client/assets/js/pages/courier/scanner.js
+++ client/assets/js/pages/courier/scanner.js
@@ -1,21 +1,15 @@
 
 Html5Qrcode.getCameras().then(devices => {
-    /**
-     * devices would be an array of objects of type:
-     * { id: "id", label: "label" }
-     */
     if (devices && devices.length) {
         const cameraId = devices.length > 1 ? devices[1].id : devices[0].id;
-
         const boxLength = screen.width / 3;
-
         const html5QrCode = new Html5Qrcode("reader");
-        html5QrCode.dim
+
         html5QrCode.start(
             cameraId,
             {
-                fps: 60,    // Optional, frame per seconds for qr code scanning
-                qrbox: { width: boxLength, height: boxLength },  // Optional, if you want bounded box UI
+                fps: 60,
+                qrbox: { width: boxLength, height: boxLength },
                 aspectRatio: 1.0
             },
             (decodedText, decodedResult) => {
@@ -27,7 +21,7 @@ Html5Qrcode.getCameras().then(devices => {
                     headers: {
                         'Content-Type': 'application/text',
                     },
-                    body: text,
+                    body: decodedText,
                 })
                     .then(data => {
                         if(data.redirected)
@@ -37,8 +31,6 @@ Html5Qrcode.getCameras().then(devices => {
                     .catch((error) => {
                         console.error('Error:', error);
                     });
-
-               // window.location = `./scan?hello=${decodedText}&babe=4`;
             },
             (errorMessage) => {
                 // parse error, ignore it.
