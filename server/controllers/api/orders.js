4865311221a6b08fd78c894042947abda273d042
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-08 14:46:55 +0200

    added email template

--- server/controllers/api/orders.js
+++ server/controllers/api/orders.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const Order = require("../../models/order");
 const Format = require("../../models/format")
 const fetch = require("node-fetch");
+const ejs = require("ejs");
+const path = require('path')
 
 router.delete("/:id", (req, res) => {
   Order.destroy({ where: { id: req.params.id } })
@@ -29,89 +31,98 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
 
-    const sendEmail = async (id) => {
-        await fetch(`https://api.hbo-ict.cloud/mail`, {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer pad_rit_6.o8ZLxKESVnaVSVQs',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "from": {
-                    "name": "Team 3",
-                    "address": "team3@hbo-ict.cloud"
-                },
-                "to": [
-                    {
-                        "name": "",
-                        "address": req.body.email
-                    }
-                ],
-                "subject": "Track & Trace",
-                "html": `Hierbij uw track & trace link: http://${req.rawHeaders[1]}/track/${id}&postal_code=${req.body.postal_code}`
-            }),
-        });
-    }
 
-    let pickup_status = req.body.is_pickup != null;
 
-    Order.create({
-        status: 'SORTING',
-        email: req.body.email,
-        weight: req.body.weight,
-        created_at: Date.now(),
-        street: req.body.street,
-        house_number: req.body.house_number,
-        postal_code: req.body.postal_code,
-        city: req.body.city,
-        country: req.body.country,
-        format: req.body.sizeFormat,
-        is_pickup: pickup_status,
-        updated_at: Date.now()})
-        .then((order) => {
-            sendEmail(order.dataValues.id);
-            res.redirect('/dashboard');
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+  const sendEmail = async () => {
+    let emailTemplate = await ejs.renderFile(path.resolve(__dirname, "../../views/mail/template.ejs"), {
+      link: `http://${req.rawHeaders[1]}/track/${req.body.id}&postal_code=${req.body.postal_code}`,
+    });
+
+    await fetch(`https://api.hbo-ict.cloud/mail`, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer pad_rit_6.o8ZLxKESVnaVSVQs',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "from": {
+          "name": "Team 6",
+          "address": "team6@hbo-ict.cloud"
+        },
+        "to": [
+          {
+            "name": "",
+            "address": req.body.email
+          }
+        ],
+        "subject": "Track & Trace",
+        "html": emailTemplate,
+      }),
+    });
+  }
+
+  let pickup_status = req.body.is_pickup != null;
+
+  Order.create({
+    status: 'SORTING',
+    email: req.body.email,
+    weight: req.body.weight,
+    created_at: Date.now(),
+    street: req.body.street,
+    house_number: req.body.house_number,
+    postal_code: req.body.postal_code,
+    city: req.body.city,
+    country: req.body.country,
+    format: req.body.sizeFormat,
+    is_pickup: pickup_status,
+    updated_at: Date.now()
+  })
+    .then((order) => {
+      sendEmail();
+      res.redirect('/dashboard');
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.post("/edit", (req, res) => {
-    let pickup_status = req.body.is_pickup != null;
+  let pickup_status = req.body.is_pickup != null;
 
-    Order.update({
-            weight: req.body.weight,
-            email: req.body.email,
-            street: req.body.street,
-            house_number: req.body.house_number,
-            postal_code: req.body.postal_code,
-            city: req.body.city,
-            format: req.body.sizeFormat,
-            is_pickup: pickup_status,
-            updated_at: Date.now(),
-            status: req.body.status},
-        {where: {id: req.body.id}})
-        .then(() => {
-            res.redirect("/dashboard");
-        })
-        .catch((err) => {
-            res.status(500).json(req.body);
-        })
+  Order.update({
+    weight: req.body.weight,
+    email: req.body.email,
+    street: req.body.street,
+    house_number: req.body.house_number,
+    postal_code: req.body.postal_code,
+    city: req.body.city,
+    format: req.body.sizeFormat,
+    is_pickup: pickup_status,
+    updated_at: Date.now(),
+    status: req.body.status
+  },
+    { where: { id: req.body.id } })
+    .then(() => {
+      res.redirect("/dashboard");
+    })
+    .catch((err) => {
+      res.status(500).json(req.body);
+    })
 })
 
 router.post("/setting", (req, res) => {
-    Format.create({
-        length: req.body.length,
-        height: req.body.height,
-        nameformat: req.body.name,
-        width: req.body.width})
-        .then((orders) => {
-            res.redirect('/dashboard/settings')
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+  Format.create({
+    length: req.body.length,
+    height: req.body.height,
+    nameformat: req.body.name,
+    width: req.body.width
+  })
+    .then((orders) => {
+      res.redirect('/dashboard/settings')
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 
