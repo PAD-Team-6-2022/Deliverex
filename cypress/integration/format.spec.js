56e2f7085c59f4796ffae9e2b83e6c63143781e4
Ilias aachboun <ili.aachboun1@gmail.com>
2022-05-31 17:04:06 +0200

    test errors fixed

--- cypress/integration/format.spec.js
+++ cypress/integration/format.spec.js
@@ -1,12 +1,22 @@
 import order from '../fixtures/order.json';
 
-describe('Log in to see the dashboard', () => {
+// check if track form shows all errors and finally submits correctly and shows tracking page
+describe('Hij logt in en dan gaat die naar settings', () => {
     it('Shows all form errors', () => {
         cy.visit('/')
         cy.visit('/dashboard/signin')
         cy.get('#username').type("Ondernemer");
         cy.get('#password').type("test");
-        cy.get('.gap-2').click();
-    });
+        cy.get('#submitSign').click();
 
+    });
+    it('Checks if the package not found taoster shows after invalid tracking code', () => {
+        cy.visit('/dashboard/settings')
+        cy.get('#name').type("XS");
+        cy.get('#length').type("12");
+        cy.get('#width').type("10");
+        cy.get('#height').type("5");
+        cy.get('#saveFormat').click();
+
+    });
 });
--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -1,21 +1,11 @@
 import order from '../fixtures/order.json';
 
-// check if track form shows all errors and finally submits correctly and shows tracking page
-describe('Hij logt in en dan gaat die naar settings', () => {
+describe('Login to go to dashboard', () => {
     it('Shows all form errors', () => {
         cy.visit('/')
         cy.visit('/dashboard/signin')
         cy.get('#username-input').type("Ondernemer");
         cy.get('#password-input').type("test");
-        cy.get('form').submit();
-
-    });
-    it('Checks if the package not found taoster shows after invalid tracking code', () => {
-        cy.visit('/dashboard/settings')
-        cy.get('#name').invoke('val', 25).trigger('change');
-        cy.get('#length').type("12");
-        cy.get('#width').type("10");
-        cy.get('#height').type("5");
-
+        cy.get('.gap-2').click();
     });
 });
