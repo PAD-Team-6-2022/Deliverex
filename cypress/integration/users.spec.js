f566dc8684a0ad70482554434364e665f69252fe
Muijf <admin@muijf.com>
2022-06-10 16:47:19 +0200

    added users test

---
+++ cypress/integration/users.spec.js
@@ -0,0 +1,45 @@
+import { username, password } from '../fixtures/ondernemer.json';
+
+describe("Log in to 'ondernemer', make a new user and log in to that user", () => {
+    it("Logs in to 'ondernemer'", () => {
+      cy.visit("/dashboard/signin")
+
+      cy.get("#username-input").type(username)
+      cy.get("#submitSign").click()
+
+      cy.get("#password-input").type(password)
+      cy.get("#submitSign").click()
+    });
+
+    it("Navigates to the users page", () => {
+      cy.get("#users-button").click();
+    });
+
+    it("Navigates to the users add page", () => {
+      cy.get("#add-user-button").click();
+    });
+
+    it("Creates a new user", () => {
+      cy.get("#username-input").type("new_test_user");
+      cy.get("#email-input").type("new_test_user@new_test_user.com");
+      cy.get("#password-input").type("password");
+      cy.get("#confirm-password-input").type("password");
+      cy.get("#submit-button").click();
+    });
+
+    it("Logs out of the current user", () => {
+      cy.get("#signout-button").click();
+    });
+
+    if("Logs in to the new user", () => {
+      cy.visit("/dashboard/signin");
+
+      cy.get("#username-input").type("new_test_user");
+      cy.get("#submitSign").click();
+
+      cy.get("#password-input").type("password");
+      cy.get("#submitSign").click();
+
+      cy.url().should('eq', '/dashboard/overview');
+    });
+});
